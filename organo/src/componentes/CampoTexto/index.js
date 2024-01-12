9a026d1235d3197f785202445792ccac2b4356c8
rwgit2023 <ronangit2023@hotmail.com>
2024-01-11 23:21:44 -0300

    cotinue to learn apis

--- organo/src/componentes/CampoTexto/index.js
+++ organo/src/componentes/CampoTexto/index.js
@@ -1,25 +1,33 @@
 import { useState } from "react";
 import "./CampoTexto.css";
 
-const CampoTexto = (propse) => {
-
-    const [valor, setValor] = useState('eu')
+const CampoTexto = (props) => {
     // let valor = 'Ronan assemiro'
+
+    // const [valor, setValor] = useState('eu')
     const aoDigitado = (evento) => {
-        setValor (evento.target.value)
-        console.log(valor)
+        // EXEMPLOS:
+        // setValor (evento.target.value)
+        // console.log(valor)
 
-    }
-
+        props.aoAlterado(evento.target.value);
+    };
 
     return (
         <div className="campo-texto">
-            <label>{propse.label}</label>
-            <input  value = {valor} onChange={aoDigitado} placeholder={propse.placeholder} required={propse.obrigatorio}></input>
-
+            <label>{props.label}</label>
+            <input
+                value={props.valor}
+                onChange={aoDigitado}
+                placeholder={props.placeholder}
+                required={props.obrigatorio}
+            ></input>
         </div>
     );
-    //required = {propse.obrigatorio}  para isso acontecer foi enviado o parâmetro no index do formulário.
 };
-
 export default CampoTexto;
+
+//required = {propse.obrigatorio}  para isso acontecer foi enviado o parâmetro no index do formulário.
+
+//useState : é um HOOK ele vai fazer alguma coisa...     essa função retorna 2 valores ->  [Value, SetValor] -> SetValor -> prepara a variável...
+//ISSO SERVE PARA RENDERIZAR ALGO...    PORQUE O REACT ENTENDE QUE NO MOMENTO QUE O ESTADO MUDA ELE PRECISA RENDERIZAR
